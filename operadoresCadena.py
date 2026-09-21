7589e5d94c85cab6b4f794bb1bcbee5abd0642fe
galletita premiun <74944677@institutourusayhua.edu.pe>
2026-09-21 11:30:41 -0500

    Add files via upload

---
+++ operadoresCadena.py
@@ -0,0 +1,19 @@
+# print("cadena 1 " + "cadena 2")
+# nom1 = "Galletita"
+# nom2 = "Alexander"
+# print(nom1 + " y " + nom2 + " : son compañeros")
+
+# nombre = input("¿Me puedes dar tu nombre por favor? ")
+# apellido = input("¿Me puedes dar tu apellido por favor? ")
+# print("Gracias. ")
+# print("\nTu nombre es " + nombre + " " + apellido + ".")
+
+print("james"*3)
+print(3* "an")
+print(5 * "2")
+
+print("+" + 30 * "🍆" + "+")
+print(("🍆" + " " * 30 + "🍆\n") * 5, end="")
+print("+" + 30 * "🍆" + "+")
+
+
